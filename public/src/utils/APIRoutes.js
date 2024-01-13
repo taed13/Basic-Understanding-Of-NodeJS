3f0f1fcc00c832522f2a056b2cfc73d9bac83c50
taed13 <taedtech13@gmail.com>
2024-01-13 17:21:44 +0700

    deploy vercel

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 // export const host = "http://localhost:1337";
-export const host = "https://chatapp-server-one.vercel.app";
+export const host = "https://project-understand-nodejs-backend.vercel.app";
 export const getAllTasksRoute = `${host}/api/v1/tasks`;
 export const createTaskRoute = `${host}/api/v1/tasks`;
 export const getTaskRoute = `${host}/api/v1/tasks`;
